44486a0ef437f79238e8a05653685035b19a9910
Mathieu Dionne <mdionne@cmaisonneuve.qc.ca>
2021-09-13 09:53:12 -0400

    Initial commit

---
+++ Assets/Scripts/DeplacementVaisseau.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeplacementVaisseau : MonoBehaviour
+{
+    public float vitesseTourne;
+    public float vitesseMonte;
+
+    public float vitesseAvant;
+    public float vitesseAvantMax;
+    public float forceAcceleration;
+
+
+  
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        
+           
+            float forceRotation = Input.GetAxis("Horizontal") * vitesseTourne * Time.deltaTime;
+            float forceMontee = Input.GetAxis("Vertical") * vitesseMonte * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.E) && vitesseAvant < vitesseAvantMax) // pour accélérer
+            {
+                vitesseAvant += forceAcceleration;
+
+            }
+
+            if (Input.GetKey(KeyCode.Q) && vitesseAvant > 0f) // pour ralentir
+            {
+
+                vitesseAvant -= forceAcceleration;
+            }
+            
+            GetComponent<Rigidbody>().AddRelativeForce(0f, forceMontee, vitesseAvant);
+            GetComponent<Rigidbody>().AddRelativeTorque(0f, forceRotation, 0f);
+
+
+            //correction au niveau de la rotation X et Z
+            transform.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y, 0f);
+
+            
+        
+
+
+
+
+
+    }
+}
